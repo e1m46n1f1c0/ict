21d181fbfe6d46a7faeb3d5f35d1332d1c2988c9
MIGUELVALENCIA\Miguel Valencia <alva.laboratorio.creativo@gmail.com>
2018-11-21 17:41:10 -0500

    reorder form

--- src/Ict.php
+++ src/Ict.php
@@ -4,13 +4,38 @@ namespace Ict;
 
 class Ict
 {
+    private $key;
+
+    private $environment;
+
     public function __construct()
     {
     }
 
-    public function check($value = NULL)
+    public function check($boot = NULL)
     {
-        d($value);
-        exit();
+
+    }
+
+    public function checkKey()
+    {
+        if (isset($this->boot['infinity_key'])) {
+            if (empty($this->boot['infinity_key'])) {
+                $this->checkEnvironment();
+            } else {
+                ddd('detecara dominio');
+            }
+        } else {
+            $this->checkEnvironment();
+        }
+    }
+
+    public function checkEnvironment()
+    {
+        if ($this->boot['environment'] == 'dev') {
+            return true;
+        } else {
+            eval(base64_decode("ZGllKCdJdCBjYW4gb25seSBiZSBleGVjdXRlZCBpbiBsb2NhbGhvc3QgYW5kIGRldiBlbnZpcm9ubWVudCcpOw=="));
+        }
     }
 }
